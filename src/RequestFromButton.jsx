a85d6c0512cd50535676c4384b05ba6dd6d246a3
bernsrepublic <bernsrepublic@gmail.com>
2023-05-23 17:57:23 -0700

    first commit

---
+++ src/RequestFromButton.jsx
@@ -0,0 +1,12 @@
+import React, { useContext } from "react";
+import StylesContext from "./StylesContext";
+
+export default function RequestFormButton() {
+  const styling = useContext(StylesContext);
+  return (
+    <div>
+      <button style={styling}>Click Here!</button>
+      <input type="submit" value="Submit" />
+    </div>
+  );
+}
